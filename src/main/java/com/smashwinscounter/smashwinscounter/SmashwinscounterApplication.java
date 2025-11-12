537c81960c3d67e9a081bb3d935fba9fd5b3401d
sebastienconde <seabasscondor@gmail.com>
2025-11-12 18:48:07 -0500

    First commit

---
+++ src/main/java/com/smashwinscounter/smashwinscounter/SmashwinscounterApplication.java
@@ -0,0 +1,13 @@
+package com.smashwinscounter.smashwinscounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashwinscounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashwinscounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smashwinscounter/smashwinscounter/SmashwinscounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smashwinscounter.smashwinscounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashwinscounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
