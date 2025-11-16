77659a9b748b60423f9b084ce8ac6b4bd4b3a49e
sebastienconde <seabasscondor@gmail.com>
2025-11-16 14:44:25 -0500

    Exposed endpoint to get character list

--- src/main/java/com/smashwinscounter/smashwinscounter/WinController.java
+++ src/main/java/com/smashwinscounter/smashwinscounter/WinController.java
@@ -2,27 +2,34 @@ package com.smashwinscounter.smashwinscounter;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/wins") // base URL
+@RequestMapping("/wins")
+@CrossOrigin(origins = "http://localhost:5173")// base URL
 public class WinController {
 
     private final WinService winService;
 
-    public WinController(WinService winService) {
-        this.winService = winService;
-    }
+    public WinController(WinService winService) { this.winService = winService; }
 
     // POST wins/add -> add a new win
     @PostMapping("/add")
-    public Win addWin(@RequestBody Win win) {
-        return winService.addWin(win);
-    }
+    public Win addWin(@RequestBody Win win) { return winService.addWin(win); }
 
     // GET /wins -> get all wins
     @GetMapping
     public List<Win> getAllWins() {
         return winService.getAllWins();
+    }
+
+    // GET /wins/characters -> get all characters
+    @GetMapping("/characters")
+    public List<CharacterDTO> getCharacters() {
+        return Arrays.stream(Character.values())
+                .map(c -> new CharacterDTO(c.name(), c.getDisplayName()))
+                .collect(Collectors.toList());
     }
 }
