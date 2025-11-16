b5f565d1f85eb4eb991fd161547bd55716060288
sebastienconde <seabasscondor@gmail.com>
2025-11-15 19:05:00 -0500

    Removed db url print statement

--- src/main/java/com/smashwinscounter/smashwinscounter/SmashWinsCounterApplication.java
+++ src/main/java/com/smashwinscounter/smashwinscounter/SmashWinsCounterApplication.java
@@ -7,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmashWinsCounterApplication {
 
 	public static void main(String[] args) {
-        // Print the DATABASE_URL for debugging (mask password)
-        String dbUrl = System.getenv("DATABASE_URL");
-        if (dbUrl != null) {
-            System.out.println("Resolved DB URL: " + dbUrl.replaceAll("password=.*?(&|$)", "password=*****$1"));
-        } else {
-            System.out.println("DATABASE_URL environment variable is not set!");
-        }
 		SpringApplication.run(SmashWinsCounterApplication.class, args);
 	}
 
